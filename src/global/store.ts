57d47b54b25ff377fe31e04406af90b57cdfcba3
Iaroslav Ponomarenko <pyummm@gmail.com>
2023-01-27 19:21:36 +0100

    .

--- src/global/store.ts
+++ src/global/store.ts
@@ -3,54 +3,53 @@ import { Store as ReduxStore } from 'redux';
 import { Store } from './interfaces';
 
 export const store = ((): Store => {
-  let _store: ReduxStore;
+    let _store: ReduxStore;
 
-  const setStore = (store: ReduxStore) => {
-    _store = store;
-  };
-
-  const getState = () => {
-    return _store && _store.getState();
-  };
-
-  const getStore = () => {
-    return _store;
-  };
-
-  const mapDispatchToProps = (component: any, props: any) => {
-    Object.keys(props).forEach(actionName => {
-      const action = props[actionName];
-      Object.defineProperty(component, actionName, {
-        get: () => (...args: any[]) => _store.dispatch(action(...args)),
-        configurable: true,
-        enumerable: true,
-      });
-    });
-  };
-
-  const mapStateToProps = (component: any, mapState: (...args: any[]) => any) => {
-    // TODO: Don't listen for each component
-    const _mapStateToProps = (_component: any, _mapState: any) => {
-      const mergeProps = mapState(_store.getState());
-      Object.keys(mergeProps).forEach(newPropName => {
-        const newPropValue = mergeProps[newPropName];
-        component[newPropName] = newPropValue;
-        // TODO: can we define new props and still have change detection work?
-      });
+    const setStore = (store: ReduxStore) => {
+        _store = store;
     };
 
-    const unsubscribe = _store.subscribe(() => _mapStateToProps(component, mapState));
+    const getState = () => {
+        return _store && _store.getState();
+    };
 
-    _mapStateToProps(component, mapState);
+    const getStore = () => {
+        return _store;
+    };
 
-    return unsubscribe;
-  };
+    const mapDispatchToProps = (component: any, props: any) => {
+        Object.keys(props).forEach(actionName => {
+            const action = props[actionName];
+            Object.defineProperty(component, actionName, {
+                get: () => (...args: any[]) => _store.dispatch(action(...args)),
+                configurable: true,
+                enumerable: true,
+            });
+        });
+    };
 
-  return {
-    getStore,
-    setStore,
-    getState,
-    mapDispatchToProps,
-    mapStateToProps,
-  };
+    const mapStateToProps = (component: any, mapState: (...args: any[]) => any) => {
+        // TODO: Don't listen for each component
+        const _mapStateToProps = (_component: any, _mapState: any) => {
+            const mergeProps = _mapState(_store.getState());
+            Object.keys(mergeProps).forEach(newPropName => {
+                _component[newPropName] = mergeProps[newPropName];
+                // TODO: can we define new props and still have change detection work?
+            });
+        };
+
+        const unsubscribe = _store.subscribe(() => _mapStateToProps(component, mapState));
+
+        _mapStateToProps(component, mapState);
+
+        return unsubscribe;
+    };
+
+    return {
+        getStore,
+        setStore,
+        getState,
+        mapDispatchToProps,
+        mapStateToProps,
+    };
 })();
